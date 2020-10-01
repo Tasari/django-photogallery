f5d15de3c0112aa9a2a453c3681d9e1b1ebc7eaa
Tasari <Tasari@wp.pl>
2020-10-01 20:19:10 +0200

    Next and previous buttons + fixed navbar

--- photogallery/urls.py
+++ photogallery/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'photogallery'
 urlpatterns = [
     path('', views.PhotoIndexView.as_view(), name='index'),
-    path('<int:pk>/', views.PhotoDetailView.as_view(), name='detail')
+    path('<int:pk>/', views.PhotoDetailView.as_view(), name='detail'),
+    path('<int:picturepost_id>/next/', views.go_to_next, name='next'),
+    path('<int:picturepost_id>/prev/', views.go_to_prev, name='prev'),
 ]
